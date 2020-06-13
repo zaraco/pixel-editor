4471b9c7c110dd3db364e389939289a2674786fe
Zaraco <zahrateymouri90@gmail.com>
2020-06-13 11:15:21 +0200

    picker listener added

--- index.js
+++ index.js
@@ -6,23 +6,30 @@ class Color {
 
 class Picker {
 
-    dom = null
+    dom = null;
 
     color = new Color();
 
     constructor() {}
 
-    attachDom(dom) {
-        this.dom = dom
-    }
+    attachDom = (dom) => {
+        this.dom = dom;
+        dom.addEventListener("click", this.onClick)
+    };
 
-    selectColor() {
+    onClick = (event) => {
+        let rect = event.target.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+    };
 
-    }
+    selectColor = (x, y) => {
+
+    };
+
 
 }
 
 const picker = new Picker();
 picker.attachDom(document.getElementById('picker'));
-console.log(picker.dom)
 
